5f6293268a31db754b1110e58988b8cac43d8988
befos7 <43826123+befos7@users.noreply.github.com>
2020-05-12 23:47:45 -0600

    ui bug fix

--- RPG_Attempt/Assets/Scripts/PlayerMovement.cs
+++ RPG_Attempt/Assets/Scripts/PlayerMovement.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -22,21 +23,21 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && !IsMouseOverUI())
         {
             //targetPos = -Vector3.one;
             targetPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10f));// 
-            
+
             Debug.Log(targetPos);
             positionSet = true;
-            
+
         }
-        if (Vector3.Distance(this.transform.position, targetPos) > 0.1f  && positionSet == true)
+        if (Vector3.Distance(this.transform.position, targetPos) > 0.1f && positionSet == true)
         {
             transform.Translate((targetPos - transform.position).normalized * (moveSpeed));
 
         }
-        else if(Vector3.Distance(this.transform.position, targetPos) < 0.1f)
+        else if (Vector3.Distance(this.transform.position, targetPos) < 0.1f)
         {
             positionSet = false;
 
@@ -50,32 +51,43 @@ public class PlayerMovement : MonoBehaviour
         }
         //
 
+        ArrowMovement();
+
+    }
+
+    private void ArrowMovement()
+    {
+        direction = Vector2.zero;
         if (Input.GetKey(KeyCode.UpArrow))
         {
             direction = Vector2.up;
-            transform.Translate(direction * moveSpeed);
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
             direction = Vector2.right;
-            transform.Translate(direction * moveSpeed);
         }
         if (Input.GetKey(KeyCode.DownArrow))
         {
             direction = Vector2.down;
-            transform.Translate(direction * moveSpeed);
         }
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             direction = Vector2.left;
-            transform.Translate(direction * moveSpeed);
         }
-
+        transform.Translate(direction * moveSpeed);
 
     }
 
-    private void OnMouseDown()
+    
+    //public void OnPointerClick(PointerEventData data)
+    //{
+    //    Debug.Log(gameObject.name + ": I was clicked!");
+    //}
+    private bool IsMouseOverUI()
     {
-      
+        return EventSystem.current.IsPointerOverGameObject();
     }
+    
+
+    
 }
