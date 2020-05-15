786152d8b03c5c0f5cc97655036dfe8d910ac749
befos7 <43826123+befos7@users.noreply.github.com>
2020-05-15 06:22:06 -0600

    minor ui

--- RPG_Attempt/Assets/Scripts/PlayerMovement.cs
+++ RPG_Attempt/Assets/Scripts/PlayerMovement.cs
@@ -11,13 +11,17 @@ public class PlayerMovement : MonoBehaviour
     private bool positionSet;
    
     private Vector2 direction;
-    private float moveSpeed = 0.1f;
+    private float directionX, directionY;
+    private float moveSpeed = 5f;
+
+    private bool menuOpen;
 
     // Start is called before the first frame update
     void Start()
     {
         targetPos = transform.position;
         positionSet = false;
+        menuOpen = false;
     }
 
     // Update is called once per frame
@@ -34,7 +38,7 @@ public class PlayerMovement : MonoBehaviour
         }
         if (Vector3.Distance(this.transform.position, targetPos) > 0.1f && positionSet == true)
         {
-            transform.Translate((targetPos - transform.position).normalized * (moveSpeed));
+            transform.Translate((targetPos - transform.position).normalized * (moveSpeed * Time.fixedDeltaTime));
 
         }
         else if (Vector3.Distance(this.transform.position, targetPos) < 0.1f)
@@ -50,35 +54,51 @@ public class PlayerMovement : MonoBehaviour
             Debug.Log(Vector3.Distance(this.transform.position, targetPos));
         }
         //
-
+        if (!menuOpen)
+        {
         ArrowMovement();
+
+        }
 
     }
 
     private void ArrowMovement()
     {
         direction = Vector2.zero;
+        directionX = 0f;
+        directionY = 0f;
         if (Input.GetKey(KeyCode.UpArrow))
         {
-            direction = Vector2.up;
+            //direction = Vector2.up;
+            directionY = 1f;
+
+
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            direction = Vector2.right;
+            //direction = Vector2.right;
+            directionX = 1f;
+
         }
         if (Input.GetKey(KeyCode.DownArrow))
         {
-            direction = Vector2.down;
+           // direction = Vector2.down;
+            directionY = -1f;
+
+
         }
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            direction = Vector2.left;
+            //direction = Vector2.left;
+            directionX = -1f;
+
         }
-        transform.Translate(direction * moveSpeed);
+        direction = new Vector2(directionX, directionY);
+        transform.Translate(direction * moveSpeed * Time.fixedDeltaTime);
 
     }
 
-    
+
     //public void OnPointerClick(PointerEventData data)
     //{
     //    Debug.Log(gameObject.name + ": I was clicked!");
@@ -86,6 +106,13 @@ public class PlayerMovement : MonoBehaviour
     private bool IsMouseOverUI()
     {
         return EventSystem.current.IsPointerOverGameObject();
+    }
+
+    public void StopMovement()
+    {
+        menuOpen = !menuOpen;
+        direction = Vector2.zero;
+        targetPos = transform.position;
     }
     
 
