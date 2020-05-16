3be7faae651a69d2ad418b54367ce027360dc563
befos7 <43826123+befos7@users.noreply.github.com>
2020-05-16 05:18:19 -0600

    talent tree panels

--- RPG_Attempt/Assets/Scripts/MenuController.cs
+++ RPG_Attempt/Assets/Scripts/MenuController.cs
@@ -7,6 +7,11 @@ public class MenuController : MonoBehaviour
 {
     [SerializeField]
     GameObject menuPanel;
+    [SerializeField]
+    private GameObject talentTreePanel;
+    [SerializeField]
+    GameObject[] talentTreePanelArray;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,5 +27,23 @@ public class MenuController : MonoBehaviour
     public void ToggleMenu()
     {
         menuPanel.SetActive(!menuPanel.activeSelf);
+    }
+
+    public void GoBackFromTalentMenu()
+    {
+        CloseTalentPanels();
+        talentTreePanel.SetActive(false);
+    }
+    public void OpenTalentPanel(int value)
+    {
+        CloseTalentPanels();
+        talentTreePanelArray[value].SetActive(true);
+    }
+    void CloseTalentPanels()
+    {
+        for (int i = 0; i < talentTreePanelArray.Length; i++)
+        {
+            talentTreePanelArray[i].SetActive(false);
+        }
     }
 }
--- RPG_Attempt/Assets/Scripts/PlayerMovement.cs
+++ RPG_Attempt/Assets/Scripts/PlayerMovement.cs
@@ -16,6 +16,7 @@ public class PlayerMovement : MonoBehaviour
 
     private bool menuOpen;
 
+
     // Start is called before the first frame update
     void Start()
     {
